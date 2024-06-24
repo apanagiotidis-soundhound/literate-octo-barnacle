b5ad9281a46fd06dee03765052a192a82095ad8c
Alexandros Panagiotidis <apanagiotidis@soundhound.com>
2024-06-24 11:29:42 +0200

    Initial commit

---
+++ src/main/java/otelcustomizer/demo/NullSpanProcessor.java
@@ -0,0 +1,30 @@
+package otelcustomizer.demo;
+
+import io.opentelemetry.context.Context;
+import io.opentelemetry.sdk.trace.ReadWriteSpan;
+import io.opentelemetry.sdk.trace.ReadableSpan;
+import io.opentelemetry.sdk.trace.SpanProcessor;
+
+public class NullSpanProcessor implements SpanProcessor {
+
+  @Override
+  public void onStart(Context context, ReadWriteSpan readWriteSpan) {
+
+  }
+
+  @Override
+  public boolean isStartRequired() {
+    return false;
+  }
+
+  @Override
+  public void onEnd(ReadableSpan readableSpan) {
+
+  }
+
+  @Override
+  public boolean isEndRequired() {
+    return false;
+  }
+
+}
---
+++ src/main/java/otelcustomizer/demo/OpenTelemetryConfiguration.java
@@ -0,0 +1,23 @@
+package otelcustomizer.demo;
+
+import io.opentelemetry.instrumentation.spring.autoconfigure.EnableOpenTelemetry;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableOpenTelemetry
+public class OpenTelemetryConfiguration {
+
+  // via https://opentelemetry.io/docs/zero-code/java/spring-boot-starter/sdk-configuration/#programmatic-configuration
+  // When this bean is enabled, then no traces will be exported at all
+//  @Bean
+  public AutoConfigurationCustomizerProvider otelCustomizer() {
+    return p ->
+        p.addSpanProcessorCustomizer(
+            (processor, config) ->
+                new NullSpanProcessor()
+        );
+  }
+
+}
