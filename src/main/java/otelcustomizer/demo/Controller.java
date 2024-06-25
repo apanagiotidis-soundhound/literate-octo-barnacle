4d98220658537d6d8a0127f37daf1b5813451b2d
Alexandros Panagiotidis <apanagiotidis@soundhound.com>
2024-06-25 14:41:35 +0200

    Remove micrometer dependencies

--- src/main/java/otelcustomizer/demo/Controller.java
+++ src/main/java/otelcustomizer/demo/Controller.java
@@ -1,8 +1,5 @@
 package otelcustomizer.demo;
 
-import io.micrometer.tracing.Tracer;
-import io.micrometer.tracing.annotation.NewSpan;
-import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,22 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-  private final Tracer tracer;
-
-  public Controller(Tracer tracer) {
-    this.tracer = tracer;
-  }
-
   @GetMapping("/hello")
-  @NewSpan
   ResponseEntity<Void> hello() {
-    try (
-        final var ignoreBag = tracer.createBaggageInScope("someId", UUID.randomUUID().toString());
-    ) {
-      tracer.currentSpan().event("Test");
-
       return ResponseEntity.ok().build();
-    }
   }
 
 }
