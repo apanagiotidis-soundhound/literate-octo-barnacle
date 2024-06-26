dbcd989f3c970b4deb331cb5cdf27ae44f4fafdc
Alexandros Panagiotidis <apanagiotidis@soundhound.com>
2024-06-26 14:34:45 +0200

    Use addTracerProviderCustomizer

--- src/main/java/otelcustomizer/demo/NullSpanProcessor.java
+++ src/main/java/otelcustomizer/demo/NullSpanProcessor.java
@@ -4,27 +4,29 @@ import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.trace.ReadWriteSpan;
 import io.opentelemetry.sdk.trace.ReadableSpan;
 import io.opentelemetry.sdk.trace.SpanProcessor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class NullSpanProcessor implements SpanProcessor {
 
   @Override
   public void onStart(Context context, ReadWriteSpan readWriteSpan) {
-
+    log.info("NullSpanProcessor.onStart");
   }
 
   @Override
   public boolean isStartRequired() {
-    return false;
+    return true;
   }
 
   @Override
   public void onEnd(ReadableSpan readableSpan) {
-
+    log.info("NullSpanProcessor.onEnd");
   }
 
   @Override
   public boolean isEndRequired() {
-    return false;
+    return true;
   }
 
 }
--- src/main/java/otelcustomizer/demo/OpenTelemetryConfiguration.java
+++ src/main/java/otelcustomizer/demo/OpenTelemetryConfiguration.java
@@ -2,6 +2,8 @@ package otelcustomizer.demo;
 
 import io.opentelemetry.instrumentation.spring.autoconfigure.EnableOpenTelemetry;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,14 +12,17 @@ import org.springframework.context.annotation.Configuration;
 public class OpenTelemetryConfiguration {
 
   // via https://opentelemetry.io/docs/zero-code/java/spring-boot-starter/sdk-configuration/#programmatic-configuration
-  // When this bean is enabled, then no traces will be exported at all
   @Bean
   public AutoConfigurationCustomizerProvider otelCustomizer() {
     return p ->
-        p.addSpanProcessorCustomizer(
-            (processor, config) ->
-                new NullSpanProcessor()
-        );
+        p.addTracerProviderCustomizer(this::configureSdkTracerProvider);
+  }
+
+  private SdkTracerProviderBuilder configureSdkTracerProvider(
+      SdkTracerProviderBuilder tracerProvider, ConfigProperties config) {
+
+    return tracerProvider
+        .addSpanProcessor(new NullSpanProcessor());
   }
 
 }
